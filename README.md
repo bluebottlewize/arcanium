5c78163303df20462aed317f7d0878c59aac88c0
bluebottlewize <143607277+bluebottlewize@users.noreply.github.com>
2024-11-27 17:47:34 +0530

    quickaccess autocomplete done

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# arcanium
-An Arcane themed browser startpage designed for speed efficiency, offering quick access to your most used sites with minimal clicks.
+# Arcanium
+An Arcane themed browser startpage designed for speed and efficiency, offering quick access to your most used sites with minimal clicks.
+
+# Screenshots
+
+![plot](./screenshots/quickaccess.png)
+
+![plot](./screenshots/search.png)
+
+![plot](./screenshots/bookmarks.png)
--- script.js
+++ script.js
@@ -6,11 +6,41 @@ let urlMap = new Map([
     ["gpt", "https://chat.openai.com/"],
 ]);
 
+const overlay = document.querySelector('#overlay');
+const qaoverlay = document.querySelector('#quickaccess-container');
+const queryoverlay = document.querySelector('#query-container');
+const quickaccess = document.querySelector('#quickaccess');
+const quickaccessSuggestion = document.querySelector('#quickaccess-suggestion');
+
+const searchbox = document.querySelector('#search-box');
+const searchinput = document.querySelector('#search-input');
+const bookmarklist = document.querySelector('#bookmark-list');
+const dialog = document.querySelector('#dialog');
+const keyinput = document.querySelector('#keyword-input');
+const urlinput = document.querySelector('#url-input');
+
+const NONE_ACTIVE = 0;
+const QUICKACCESS_FRAGMENT = 1;
+const SEARCH_FRAGMENT = 2;
+const BOOKMARK_FRAGMENT = 3;
+
+let activeFragment = SEARCH_FRAGMENT;
+
+let isOverlayActive = false;
+let isQuickAccessOverlayActive = false;
+let isQueryOverlayActive = false;
+let isDialogActive = false;
+
 // util functions
 
-function findKeyStartingWith(input, map)
+function findKeyStartingWith(input)
 {
-    for (let [key, value] of map)
+    if (!input)
+    {
+        return input;
+    }
+
+    for (let [key, value] of urlMap)
     {
         if (key.toLowerCase().startsWith(input.toLowerCase()))
         {
@@ -64,8 +94,8 @@ function showFilteredBookmarks(filter)
     console.log(listItemTemplate);
 
 
-
-    result.forEach((element, index) => {
+    result.forEach((element, index) =>
+    {
         let listItemClone = listItemTemplate.content.cloneNode(true);
 
         const itemTitle = listItemClone.querySelector(".bookmark-title");
@@ -80,23 +110,6 @@ function showFilteredBookmarks(filter)
 }
 
 getUrls();
-
-const overlay = document.querySelector('#overlay');
-const qaoverlay = document.querySelector('#quickaccess-container');
-const queryoverlay = document.querySelector('#query-container');
-const quickaccess = document.querySelector('#quickaccess');
-const searchbox = document.querySelector('#search-box');
-const searchinput = document.querySelector('#search-input');
-const bookmarklist = document.querySelector('#bookmark-list');
-const dialog = document.querySelector('#dialog');
-const keyinput = document.querySelector('#keyword-input');
-const urlinput = document.querySelector('#url-input');
-
-
-let isOverlayActive = false;
-let isQuickAccessOverlayActive = false;
-let isQueryOverlayActive = false;
-let isDialogActive = false;
 
 quickaccess.focus();
 
@@ -113,25 +126,127 @@ function closeEverything()
     searchbox.classList.remove("active");
     dialog.classList.remove("active");
     bookmarklist.classList.remove("active");
+
     searchinput.value = "";
     quickaccess.value = "";
+    quickaccessSuggestion.value = "";
     keyinput.value = "";
     urlinput.value = "";
 
+    quickaccess.style.textIndent = "0ch";
+
     clearBookmarkList();
 
+    activeFragment = NONE_ACTIVE;
+    searchinput.focus();
+}
+
+function openSearch()
+{
+    isOverlayActive = true;
+    isQueryOverlayActive = true;
+    activeFragment = SEARCH_FRAGMENT;
+
+    overlay.classList.add("active");
+    searchbox.classList.add("active");
+
+    searchinput.focus();
+    console.log(event.key);
+}
+
+function openBookmarkList()
+{
+    isDialogActive = true;
+    activeFragment = BOOKMARK_FRAGMENT;
+
+    overlay.classList.add("active");
+    dialog.classList.add("active");
+    bookmarklist.classList.add("active");
+
+    showFilteredBookmarks("");
+
+    keyinput.focus();
+}
+
+function openQuickAccess()
+{
+    isOverlayActive = true;
+    isQuickAccessOverlayActive = true;
+    activeFragment = QUICKACCESS_FRAGMENT;
+
+    overlay.classList.add("active");
+    console.log(event.key);
+
+    quickaccess.value = "";
     quickaccess.focus();
+}
+
+function saveBookmark()
+{
+    let key = keyinput.value;
+    let url = urlinput.value;
+
+    if (key && url)
+    {
+        urlMap.set(key, url);
+    }
+
+    storeUrls();
 }
 
 document.addEventListener("keydown", function (event)
 {
-    if (isOverlayActive && !isDialogActive)
+    if (event.key === "Escape")
     {
-        if (event.key === "Escape")
+        closeEverything();
+        return;
+    }
+
+    if (activeFragment === NONE_ACTIVE)
+    {
+        if (event.key.length === 1 && event.key === "/" && document.activeElement.value.length === 0)
         {
             closeEverything();
+            openQuickAccess();
         }
-        if (event.ctrlKey && event.key === "Enter")
+        else if (event.key.length === 1)
+        {
+            openSearch();
+        }
+        else if (event.ctrlKey && event.key === "Alt")
+        {
+            openBookmarkList();
+        }
+    }
+    else if (activeFragment === QUICKACCESS_FRAGMENT)
+    {
+        if (event.key === "Enter")
+        {
+            let query = findKeyStartingWith(quickaccess.value.substring(1));
+
+            console.log(query);
+
+            if (query != null)
+            {
+                closeEverything();
+                window.location.href = urlMap.get(query);
+            }
+            else
+            {
+                closeEverything();
+            }
+
+            quickaccess.value = "";
+        }
+    }
+    else if (activeFragment === SEARCH_FRAGMENT)
+    {
+        if (event.key === "/" && document.activeElement.value.length === 0)
+        {
+            closeEverything();
+            openQuickAccess();
+        }
+        else if (event.ctrlKey && event.key === "Enter")
         {
             let query = searchinput.value;
             console.log(query);
@@ -140,104 +255,67 @@ document.addEventListener("keydown", function (event)
         }
         else if (event.key === "Enter")
         {
-            if (isQuickAccessOverlayActive)
-            {
-                let query = findKeyStartingWith(quickaccess.value, urlMap);
-
-                console.log(query);
-
-                if (query != null)
-                {
-                    closeEverything();
-                    window.location.href = urlMap.get(query);
-                }
-                else
-                {
-                    closeEverything();
-                }
-
-                quickaccess.value = "";
-            }
-            else if (isQueryOverlayActive)
-            {
-                let query = searchinput.value;
-                console.log(query);
-                closeEverything();
-                window.location.href = "https://duckduckgo.com/?q=" + query;
-            }
-
-            quickaccess.focus();
-        }
-        else if (isQuickAccessOverlayActive)
-        {
-            quickaccess.focus();
+            let query = searchinput.value;
+            console.log(query);
+            closeEverything();
+            window.location.href = "https://duckduckgo.com/?q=" + query;
         }
     }
-    else if (!isOverlayActive && !isDialogActive)
-    {
-        if (event.key.length === 1)
-        {
-            isOverlayActive = true;
-            isQuickAccessOverlayActive = true;
-
-            overlay.classList.add("active");
-            console.log(event.key);
-        }
-        else if (event.key === "Shift")
-        {
-            isOverlayActive = true;
-            isQueryOverlayActive = true;
-
-            overlay.classList.add("active");
-            searchbox.classList.add("active");
-
-            searchinput.focus();
-            console.log(event.key);
-        }
-        else if (event.ctrlKey && event.key === "Alt")
-        {
-            isDialogActive = true;
-
-            overlay.classList.add("active");
-            dialog.classList.add("active");
-            bookmarklist.classList.add("active");
-
-            showFilteredBookmarks("");
-
-            keyinput.focus();
-        }
-    }
-    else if (isDialogActive)
+    else if (activeFragment === BOOKMARK_FRAGMENT)
     {
         if (event.key === "Enter")
         {
-            let key = keyinput.value;
-            let url = urlinput.value;
-
-            if (key && url)
-            {
-                urlMap.set(key, url);
-            }
-
-            storeUrls();
-
-            closeEverything();
-        }
-        else if (event.key === "Escape")
-        {
+            saveBookmark();
             closeEverything();
         }
     }
+});
 
-    const key = event.keyCode || event.which;
+quickaccess.addEventListener("input", function ()
+{
+    let qa = quickaccess.value.trim().replace(/\s+/g, "");
+    quickaccess.value = quickaccess.value.toLowerCase();
 
-    // if (key < 65 || (key > 65 + 26 && key < 97) || key > 97 + 26)
-    // {
-    //     if (event.preventDefault) event.preventDefault();
-    // }
+    if (quickaccess.value.length === 0)
+    {
+        closeEverything();
+        return;
+    }
+    else if (quickaccess.value.length < 2)
+    {
+        quickaccessSuggestion.value = "";
+        quickaccess.style.textIndent = "0ch";
+        return;
+    }
+
+    console.log();
+
+    let result = findKeyStartingWith(qa.substring(1));
+
+    if (result == null)
+    {
+        quickaccessSuggestion.value = "";
+        quickaccess.style.textIndent = "0ch";
+        return;
+    }
+
+    quickaccessSuggestion.value = "/" + findKeyStartingWith(qa.substring(1));
+    quickaccess.style.textIndent = "-" + (quickaccessSuggestion.value.length - quickaccess.value.length) + "ch";
 });
 
 keyinput.addEventListener('input', function ()
 {
     showFilteredBookmarks(keyinput.value);
 });
+
+window.onload = function ()
+{
+    isOverlayActive = true;
+    isQueryOverlayActive = true;
+
+    overlay.classList.add("active");
+    searchbox.classList.add("active");
+
+    searchinput.focus();
+    console.log(event.key);
+}
